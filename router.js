23b7f306dd726dcea2287f7f67670d8c33a11948
yauhenikazuseu <kazusev2000@mail.ru>
2022-12-14 19:32:19 +0300

    add show dir content

--- router.js
+++ router.js
@@ -3,12 +3,14 @@ import { homedir } from 'os';
 import { writeCurrDirInfoMessage } from './src/utils/writeCurrDirInfoMessage.js';
 import { moveUp } from './src/modules/navigation/move-up.js';
 import { changeDir } from './src/modules/navigation/change-dir.js';
+import { showDirContent } from './src/modules/navigation/show-dir-content.js';
 
 let currDir = homedir();
 const routes = {
   goUpper: 'up',
   exit: '.exit',
   changeDir: 'cd',
+  showDirContent: 'ls',
 };
 
 export async function routerController(data) {
@@ -24,6 +26,9 @@ export async function routerController(data) {
       break;
     case routes.changeDir:
       currDir = await changeDir(currDir, commandArgs[1]);
+      break;
+    case routes.showDirContent:
+      await showDirContent(currDir);
       break;
     default:
       writeCurrDirInfoMessage(currDir);
